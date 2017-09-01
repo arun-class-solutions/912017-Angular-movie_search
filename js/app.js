8a50ed2329fe73b52ab1167cf32538d5a8fc55ee
Arun Sood <arsood@gmail.com>
2017-09-01 09:50:39 -0700

    Routing working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("movieApp", []);
+var app = angular.module("movieApp", ["ngRoute"]);
+
+// Configure routing for the entire application
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/movies", {
+            templateUrl: "templates/movie-list.html",
+            controller: "movieCtrl"
+        })
+        .otherwise({
+            redirectTo: "/movies"
+        });
+});
+
+// Main application controller
+app.controller("movieCtrl", function($scope, $http) {
+
+});
